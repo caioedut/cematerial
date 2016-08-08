0449a28faa4f2f7ba45ffcc0600f84813b5e885f
Caio Eduardo <caioedut@gmail.com>
2016-08-08 18:41:16 -0300

    jQuery Version

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=2.0"/>
 
     <!-- JQUERY -->
-    <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
 
     <!-- FONTS -->
     <link type="text/css" rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto"/>

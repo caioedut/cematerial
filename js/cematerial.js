fe04dbad8876c5c8f2107285e8737e7104586761
Caio Eduardo <caioedut@outlook.com>
2016-08-29 17:03:29 -0300

    Fix waves

--- js/cematerial.js
+++ js/cematerial.js
@@ -1,285 +1,292 @@
 // INIT CEMATERIAL
-jQuery(function($) {
-  var doc = $(document);
-  CEMaterial.init(doc);
-  doc.on('DOMNodeInserted', function (e) {
-    CEMaterial.init($(e.target));
-  });
+jQuery(function ($) {
+    var doc = $(document);
+    CEMaterial.init(doc);
+    doc.on('DOMNodeInserted', function (e) {
+        CEMaterial.init($(e.target));
+    });
 });
 
 jQuery(function ($) {
-  var doc = $(document);
-  var body = $('body');
-  var app = body;
+    var doc = $(document);
+    var body = $('body');
+    var app = body;
 
-  // CLASS BINDING
-  app
-  .on('focus', '[data-class-focus]', function () {
-    CEMaterial.bindClassEvent($(this), 'focus');
-  })
-  .on('blur', '[data-class-focus]', function () {
-    CEMaterial.unbindClassEvent($(this), 'focus');
-  })
-  .on('mouseenter', '[data-class-hover]', function () {
-    CEMaterial.bindClassEvent($(this), 'hover');
-  })
-  .on('mouseleave', '[data-class-hover]', function () {
-    CEMaterial.unbindClassEvent($(this), 'hover');
-  })
-  ;
-
-  // LABEL TOGGLE
-  var texts = 'input:not([type="radio"]):not([type="checkbox"]):not([type="button"]), select, textarea';
-  app
-  .on('focus', texts, function () {
-    CEMaterial.onFocus($(this));
-  })
-  .on('blur', texts, function () {
-    CEMaterial.onBlur($(this));
-  })
-  ;
-
-  app
-  .on('click', '.waves', function (e) {
-    var el = $(this);
-    var box = el.find('.waves-box');
-
-    if (!box.length) {
-      box = $('<div class="waves-box"><span></span></div>').appendTo(el);
-    }
-
-    var wave = $('<span></span>');
-    box.find('span').replaceWith(wave);
-
-    // Get size
-    var size = Math.max(el.outerWidth(), el.outerHeight()) * 2;
-    var offset = el.offset();
-
-    wave
-    .css({
-      left: e.pageX - offset.left,
-      top: e.pageY - offset.top,
-      height: size,
-      width: size,
-      opacity: .4
-    })
-    .on('transitionend', function () {
-      if ($(this).css('opacity') == 0.4) {
-        wave.css('opacity', 0);
-      }
-    })
+    // CLASS BINDING
+    app
+        .on('focus', '[data-class-focus]', function () {
+            CEMaterial.bindClassEvent($(this), 'focus');
+        })
+        .on('blur', '[data-class-focus]', function () {
+            CEMaterial.unbindClassEvent($(this), 'focus');
+        })
+        .on('mouseenter', '[data-class-hover]', function () {
+            CEMaterial.bindClassEvent($(this), 'hover');
+        })
+        .on('mouseleave', '[data-class-hover]', function () {
+            CEMaterial.unbindClassEvent($(this), 'hover');
+        })
     ;
 
-    return true;
-  });
-  ;
+    // LABEL TOGGLE
+    var texts = 'input:not([type="radio"]):not([type="checkbox"]):not([type="button"]), select, textarea';
+    app
+        .on('focus', texts, function () {
+            CEMaterial.onFocus($(this));
+        })
+        .on('blur', texts, function () {
+            CEMaterial.onBlur($(this));
+        })
+    ;
 
-  // TEXTAREA AUTO GROW
-  app.on('input', '.input-autogrow', function() {
-    var el = $(this);
-    CEMaterial.inputAutoGrow(el);
-  });
+    app
+        .on('mouseup mouseleave', '.waves', function () {
+            var wave = $(this).find('.waves-box span');
 
-  // DRAG AND DROP UPLOAD
-  app
-  .on('drag dragstart dragend dragover dragenter dragleave drop', '.filedrop', function (e) {
-    e.preventDefault();
-    e.stopPropagation();
-  })
-  .on('dragover dragenter', '.filedrop', function () {
-    $(this).addClass('filedrop-dragover');
-  })
-  .on('dragleave drop', '.filedrop', function () {
-    $(this).removeClass('filedrop-dragover');
-  })
-  .on('drop', '.filedrop', function (e) {
-    e.preventDefault();
+            if (wave.css('opacity') == 0.4) {
+                wave.css('opacity', 0);
+            } else {
+                wave.one('transitionend', function () {
+                    wave.css('opacity', 0);
+                });
+            }
+        })
+        .on('mousedown', '.waves', function (e) {
+            var el = $(this);
+            var box = el.find('.waves-box');
 
-    var el = $(this);
-    var input = el.find('input[type="file"]');
-    var files = e.originalEvent.dataTransfer.files || [];
-    var accept = (input.attr('accept') || "").split(',');
+            if (!box.length) {
+                box = $('<div class="waves-box"><span></span></div>').appendTo(el);
+            }
 
-    input.prop('files', files);
-  })
-  .on('change', '.filedrop input[type="file"]', function (e) {
-    var input = $(this);
-    var el = input.closest('.filedrop');
-    var files = e.target.files;
+            var wave = box.find('span').attr('style', 'transition: none;');
 
-    el.find('img, .filedrop-list').remove();
+            // Get size
+            var size = Math.max(el.outerWidth(), el.outerHeight()) * 2;
+            var offset = el.offset();
 
-    if (files && files.length) {
-      var list = $('<div class="filedrop-list"></div>').prependTo(el);
+            wave
+                .attr('style', '')
+                .css({
+                    left: e.pageX - offset.left,
+                    top: e.pageY - offset.top,
+                    height: size,
+                    width: size,
+                    opacity: .4
+                })
+            ;
 
-      for (var i in files) {
-        if (isNaN(i)) {
-          break;
+            return true;
+        })
+    ;
+
+    // TEXTAREA AUTO GROW
+    app.on('input', '.input-autogrow', function () {
+        var el = $(this);
+        CEMaterial.inputAutoGrow(el);
+    });
+
+    // DRAG AND DROP UPLOAD
+    app
+        .on('drag dragstart dragend dragover dragenter dragleave drop', '.filedrop', function (e) {
+            e.preventDefault();
+            e.stopPropagation();
+        })
+        .on('dragover dragenter', '.filedrop', function () {
+            $(this).addClass('filedrop-dragover');
+        })
+        .on('dragleave drop', '.filedrop', function () {
+            $(this).removeClass('filedrop-dragover');
+        })
+        .on('drop', '.filedrop', function (e) {
+            e.preventDefault();
+
+            var el = $(this);
+            var input = el.find('input[type="file"]');
+            var files = e.originalEvent.dataTransfer.files || [];
+            var accept = (input.attr('accept') || "").split(',');
+
+            input.prop('files', files);
+        })
+        .on('change', '.filedrop input[type="file"]', function (e) {
+            var input = $(this);
+            var el = input.closest('.filedrop');
+            var files = e.target.files;
+
+            el.find('img, .filedrop-list').remove();
+
+            if (files && files.length) {
+                var list = $('<div class="filedrop-list"></div>').prependTo(el);
+
+                for (var i in files) {
+                    if (isNaN(i)) {
+                        break;
+                    }
+
+                    var reader = new FileReader();
+                    reader.onload = function (e) {
+                        list.append('<img class="img-fluid" src="' + e.target.result + '"/>');
+                    };
+                    reader.readAsDataURL(files[i]);
+                }
+            }
+        })
+    ;
+
+    // SUBMENU CLICK REPLACING HOVER
+    app.on('click', '.menu li a', function (e) {
+        var el = $(this);
+        var target = el.closest('li').find('> ul');
+
+        if (target.length) {
+            if (!el.data('toggle')) {
+                el
+                    .attr('data-toggle', 'menu')
+                    .data('target', target)
+                ;
+
+                $('.menu-visible').not(el.parents('.menu-visible')).not(target).removeClass('menu-visible');
+                target.toggleClass('menu-visible');
+                e.stopPropagation();
+            }
         }
+    });
 
-        var reader = new FileReader();
-        reader.onload = function (e) {
-          list.append('<img class="img-fluid" src="' + e.target.result + '"/>');
-        };
-        reader.readAsDataURL(files[i]);
-      }
-    }
-  })
-  ;
+    // MODAL EVENTS
+    app.on('click', '.dialog-visible', function (e) {
+        var dialog = $(this);
+        var target = $(e.target);
 
-  // SUBMENU CLICK REPLACING HOVER
-  app.on('click', '.menu li a', function (e) {
-    var el = $(this);
-    var target = el.closest('li').find('> ul');
+        if (dialog.is(target)) {
+            dialog.removeClass('dialog-visible');
+        }
+    });
 
-    if (target.length) {
-      if (!el.data('toggle')) {
-        el
-        .attr('data-toggle', 'menu')
-        .data('target', target)
-        ;
+    // DATA TOGGLE
+    app.on('click', '[data-toggle]', function (e) {
+        var el = $(this);
+        var action = el.data('toggle').trim();
+        var target = CEMaterial.getTarget(el);
 
-        $('.menu-visible').not(el.parents('.menu-visible')).not(target).removeClass('menu-visible');
-        target.toggleClass('menu-visible');
-        e.stopPropagation();
-      }
-    }
-  });
+        switch (action) {
+            case 'sidebar':
+                target.toggleClass('sidebar-visible');
+                e.stopPropagation();
+                break;
+            case 'menu':
+                $('.menu-visible').not(el.parents('.menu-visible')).not(target).removeClass('menu-visible');
+                target.toggleClass('menu-visible');
+                e.stopPropagation();
+                break;
+            case 'dialog':
+                target = target.length ? target : el.closest('.dialog');
+                target.toggleClass('dialog-visible');
 
-  // MODAL EVENTS
-  app.on('click', '.dialog-visible', function (e) {
-    var dialog = $(this);
-    var target = $(e.target);
+                if (el.data('focus')) {
+                    target.one('transitionend', function () {
+                        target.find(el.data('focus')).focus();
+                    });
+                }
 
-    if (dialog.is(target)) {
-      dialog.removeClass('dialog-visible');
-    }
-  });
+                e.stopPropagation();
+                break;
+            case 'table':
+                var checked = el.prop('checked');
+                target = target.length ? target : el.closest('table');
+                target.find('input[type="checkbox"]').prop('checked', checked);
+                e.stopPropagation();
+                break;
+            case 'panel':
+                target = target.length ? target : el.closest('.panel');
+                el.closest('.panel-group').find('.panel').not(target).removeClass('panel-visible');
+                target.toggleClass('panel-visible');
+                e.stopPropagation();
+                break;
+            default:
+                break;
+        }
+    });
 
-  // DATA TOGGLE
-  app.on('click', '[data-toggle]', function (e) {
-    var el = $(this);
-    var action = el.data('toggle').trim();
-    var target = CEMaterial.getTarget(el);
-
-    switch (action) {
-      case 'sidebar':
-      target.toggleClass('sidebar-visible');
-      e.stopPropagation();
-      break;
-      case 'menu':
-      $('.menu-visible').not(el.parents('.menu-visible')).not(target).removeClass('menu-visible');
-      target.toggleClass('menu-visible');
-      e.stopPropagation();
-      break;
-      case 'dialog':
-      target = target.length ? target : el.closest('.dialog');
-      target.toggleClass('dialog-visible');
-
-      if (el.data('focus')) {
-        target.one('transitionend', function () {
-          target.find(el.data('focus')).focus();
-        });
-      }
-
-      e.stopPropagation();
-      break;
-      case 'table':
-      var checked = el.prop('checked');
-      target = target.length ? target : el.closest('table');
-      target.find('input[type="checkbox"]').prop('checked', checked);
-      e.stopPropagation();
-      break;
-      case 'panel':
-      target = target.length ? target : el.closest('.panel');
-      el.closest('.panel-group').find('.panel').not(target).removeClass('panel-visible');
-      target.toggleClass('panel-visible');
-      e.stopPropagation();
-      break;
-      default:
-      break;
-    }
-  });
-
-  // CLOSE SIDEBARS/MENUS/DIALOGS ON BODY CLICK (MUST BE THE LAST EVENT)
-  doc.on('click', function (e) {
-    var target = $(e.target);
-    $('.sidebar-visible').not(target.closest('.sidebar-visible')).removeClass('sidebar-visible');
-    $('.menu-visible').not(target.closest('.menu-visible')).removeClass('menu-visible');
-  });
+    // CLOSE SIDEBARS/MENUS/DIALOGS ON BODY CLICK (MUST BE THE LAST EVENT)
+    doc.on('click', function (e) {
+        var target = $(e.target);
+        $('.sidebar-visible').not(target.closest('.sidebar-visible')).removeClass('sidebar-visible');
+        $('.menu-visible').not(target.closest('.menu-visible')).removeClass('menu-visible');
+    });
 
 });
 
 
 var CEMaterial = {
-  init: function(target) {
-    CEMaterial.onBlur(target.find('.label-float .input').not(':button').filter(function () {
-      return this.value;
-    }));
-    CEMaterial.inputAutoGrow(target.find('.input-autogrow'));
-  },
-  getTarget: function (el) {
-    if (el.data('target')) {
-      return $(el.data('target'));
-    } else if (el.attr('href')) {
-      return $(el.attr('href'));
+    init: function (target) {
+        CEMaterial.onBlur(target.find('.label-float .input').not(':button').filter(function () {
+            return this.value;
+        }));
+        CEMaterial.inputAutoGrow(target.find('.input-autogrow'));
+    },
+    getTarget: function (el) {
+        if (el.data('target')) {
+            return $(el.data('target'));
+        } else if (el.attr('href')) {
+            return $(el.attr('href'));
+        }
+
+        return new jQuery();
+    },
+    getLabels: function (el) {
+        var label = el.closest('label,.label');
+        var id = el.attr('id');
+
+        if (id) {
+            label = label.add($('label[for="' + id + '"]'));
+        }
+
+        return label;
+    },
+    onFocus: function (els) {
+        if (els.length) {
+            els.each(function () {
+                var el = $(this);
+                var label = CEMaterial.getLabels(el);
+                label.addClass('label-active label-focus');
+            });
+        }
+    },
+    onBlur: function (els) {
+        if (els.length) {
+            els.each(function () {
+                var el = $(this);
+                var label = CEMaterial.getLabels(el).removeClass('label-focus');
+
+                // Check LABEL FLOATING
+                var value = el.val() || '';
+                var has_value = value instanceof Array ? value.length : value.trim();
+
+                label.toggleClass('label-active', has_value ? true : false);
+            });
+        }
+    },
+    bindClassEvent: function (el, event) {
+        var clazz = el.data('class-' + event);
+
+        if (!el.hasClass(clazz)) {
+            el.data(event + '-remove', clazz);
+            el.addClass(clazz);
+        }
+    },
+    unbindClassEvent: function (el, event) {
+        el.removeClass(el.data(event + '-remove'));
+    },
+    inputAutoGrow: function (els) {
+        els.each(function () {
+            var el = $(this);
+            if (el.is('textarea')) {
+                this.style.height = '';
+                /* Reset the height*/
+                this.style.height = this.scrollHeight + 'px';
+            } else if (el.is('input') && el.val()) {
+                el.prop('size', el.val().length);
+            }
+        });
     }
-
-    return new jQuery();
-  },
-  getLabels: function (el) {
-    var label = el.closest('label,.label');
-    var id = el.attr('id');
-
-    if (id) {
-      label = label.add($('label[for="' + id + '"]'));
-    }
-
-    return label;
-  },
-  onFocus: function (els) {
-    if (els.length) {
-      els.each(function () {
-        var el = $(this);
-        var label = CEMaterial.getLabels(el);
-        label.addClass('label-active label-focus');
-      });
-    }
-  },
-  onBlur: function (els) {
-    if (els.length) {
-      els.each(function () {
-        var el = $(this);
-        var label = CEMaterial.getLabels(el).removeClass('label-focus');
-
-        // Check LABEL FLOATING
-        var value = el.val() || '';
-        var has_value = value instanceof Array ? value.length : value.trim();
-
-        label.toggleClass('label-active', has_value ? true : false);
-      });
-    }
-  },
-  bindClassEvent: function (el, event) {
-    var clazz = el.data('class-' + event);
-
-    if (!el.hasClass(clazz)) {
-      el.data(event + '-remove', clazz);
-      el.addClass(clazz);
-    }
-  },
-  unbindClassEvent: function (el, event) {
-    el.removeClass(el.data(event + '-remove'));
-  },
-  inputAutoGrow: function(els) {
-    els.each(function() {
-      var el = $(this);
-      if (el.is('textarea')) {
-        this.style.height = ''; /* Reset the height*/
-        this.style.height = this.scrollHeight + 'px';
-      } else if (el.is('input') && el.val()) {
-        el.prop('size', el.val().length);
-      }
-    });
-  }
 };

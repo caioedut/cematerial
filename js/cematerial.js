8f286783bb33b22d5f8e1d0f71fc5bc735646c86
Caio Eduardo <caioedut@gmail.com>
2016-11-01 14:48:34 -0200

    Waves alpha

--- js/cematerial.js
+++ js/cematerial.js
@@ -1059,6 +1059,62 @@ $.fn.scrollParent = function (horizontal, vertical) {
 }(jQuery);
 
 
+/** ========================================================================
+ *
+ * CEMaterial Waves
+ *
+ * ======================================================================== */
+
++function ($) {
+
+    'use strict';
+
+    $(document).on('click', '.waves', function (e) {
+
+        var $el = $(this);
+        var $wave = $el.find('.waves-body');
+
+        if (!$wave.length) {
+            $wave = $('<div class="waves-body"/>').appendTo($el);
+        }
+
+        if ($el.hasClass('waves-dark')) {
+            $wave.addClass('bg-black');
+        }
+
+        var offset_x = e.pageX - $el.offset().left;
+        var offset_y = e.pageY - $el.offset().top;
+
+        var size = Math.max($el.outerHeight(), $el.outerWidth()) * 2;
+
+        $wave
+            .addClass('no-transition')
+            .css({
+                top: offset_y,
+                left: offset_x,
+                opacity: '',
+                height: 0,
+                width: 0
+            });
+
+        setTimeout(function () {
+            $wave
+                .removeClass('no-transition')
+                .css({
+                    height: size,
+                    width: size
+                })
+                .one('transitionend', function () {
+                    $wave.css('opacity', '0');
+                })
+            ;
+        }, 1);
+
+    });
+
+}(jQuery);
+
+
 // INIT CEMATERIAL
 jQuery(function ($) {
     var doc = $(document);
@@ -1066,6 +1122,9 @@ jQuery(function ($) {
     doc.on('DOMNodeInserted', function (e) {
         CEMaterial.init($(e.target));
     });
+
+    // Waves.attach('.waves');
+    // Waves.init();
 });
 
 jQuery(function ($) {
---
+++ js/plugins/cem.waves.js
@@ -0,0 +1,54 @@
+/** ========================================================================
+ *
+ * CEMaterial Waves
+ *
+ * ======================================================================== */
+
++function ($) {
+
+    'use strict';
+
+    $(document).on('click', '.waves', function (e) {
+
+        var $el = $(this);
+        var $wave = $el.find('.waves-body');
+
+        if (!$wave.length) {
+            $wave = $('<div class="waves-body"/>').appendTo($el);
+        }
+
+        if ($el.hasClass('waves-dark')) {
+            $wave.addClass('bg-black');
+        }
+
+        var offset_x = e.pageX - $el.offset().left;
+        var offset_y = e.pageY - $el.offset().top;
+
+        var size = Math.max($el.outerHeight(), $el.outerWidth()) * 2;
+
+        $wave
+            .addClass('no-transition')
+            .css({
+                top: offset_y,
+                left: offset_x,
+                opacity: '',
+                height: 0,
+                width: 0
+            });
+
+        setTimeout(function () {
+            $wave
+                .removeClass('no-transition')
+                .css({
+                    height: size,
+                    width: size
+                })
+                .one('transitionend', function () {
+                    $wave.css('opacity', '0');
+                })
+            ;
+        }, 1);
+
+    });
+
+}(jQuery);

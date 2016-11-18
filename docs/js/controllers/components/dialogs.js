1fc2d2ddec793a49cf782e18b6c8d073fc1b7176
Caio Eduardo <caioedut@gmail.com>
2016-11-18 21:05:35 -0200

    Dialogs

--- docs/js/controllers/components/dialogs.js
+++ docs/js/controllers/components/dialogs.js
@@ -7,21 +7,19 @@ angular.module('docs').controller('components.dialogsCtrl', function ($scope) {
             '    Open Dialog' + "\n" +
             '</button>' + "\n" +
             '<div id="dialog1" class="dialog">' + "\n" +
-            '    <div class="dialog-content">' + "\n" +
-            '        <div class="dialog-header">' + "\n" +
-            '            <h3 class="dialog-title">My Dialog</h3>' + "\n" +
-            '        </div>' + "\n" +
-            '        <div class="dialog-body">' + "\n" +
-            '            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam aspernatur, delectus deserunt earum error fugit iusto qui sit ut veniam. Alias at cupiditate enim est et iste necessitatibus neque ut.' + "\n" +
-            '        </div>' + "\n" +
-            '        <div class="dialog-footer">' + "\n" +
-            '            <button class="btn btn-flat text-blue-6" type="button" data-toggle="dialog">' + "\n" +
-            '                Cancel' + "\n" +
-            '            </button>' + "\n" +
-            '            <button class="btn btn-flat text-blue-6" type="button">' + "\n" +
-            '                Confirm' + "\n" +
-            '            </button>' + "\n" +
-            '        </div>' + "\n" +
+            '    <div class="dialog-header">' + "\n" +
+            '        <h3 class="dialog-title">My Dialog</h3>' + "\n" +
+            '    </div>' + "\n" +
+            '    <div class="dialog-body">' + "\n" +
+            '        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam aspernatur, delectus deserunt earum error fugit iusto qui sit ut veniam. Alias at cupiditate enim est et iste necessitatibus neque ut.' + "\n" +
+            '    </div>' + "\n" +
+            '    <div class="dialog-footer">' + "\n" +
+            '        <button class="btn btn-flat text-blue-6" type="button" data-toggle="dialog">' + "\n" +
+            '            Cancel' + "\n" +
+            '        </button>' + "\n" +
+            '        <button class="btn btn-flat text-blue-6" type="button">' + "\n" +
+            '            Confirm' + "\n" +
+            '        </button>' + "\n" +
             '    </div>' + "\n" +
             '</div>'
         ),
@@ -29,22 +27,20 @@ angular.module('docs').controller('components.dialogsCtrl', function ($scope) {
             '<button class="btn bg-blue-6" type="button" data-toggle="dialog" data-target="#dialog2">' + "\n" +
             '    Open Dialog' + "\n" +
             '</button>' + "\n" +
-            '<div id="dialog2" class="dialog">' + "\n" +
-            '    <div class="dialog-content xs-col-10 sm-col-8 md-col-6">' + "\n" +
-            '        <div class="dialog-header">' + "\n" +
-            '            <h3 class="dialog-title">My Dialog</h3>' + "\n" +
-            '        </div>' + "\n" +
-            '        <div class="dialog-body">' + "\n" +
-            '            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam aspernatur, delectus deserunt earum error fugit iusto qui sit ut veniam. Alias at cupiditate enim est et iste necessitatibus neque ut.' + "\n" +
-            '        </div>' + "\n" +
-            '        <div class="dialog-footer">' + "\n" +
-            '            <button class="btn btn-flat text-blue-6" type="button" data-toggle="dialog">' + "\n" +
-            '                Cancel' + "\n" +
-            '            </button>' + "\n" +
-            '            <button class="btn btn-flat text-blue-6" type="button">' + "\n" +
-            '                Confirm' + "\n" +
-            '            </button>' + "\n" +
-            '        </div>' + "\n" +
+            '<div id="dialog2" class="dialog xs-col-10 sm-col-8 md-col-6">' + "\n" +
+            '    <div class="dialog-header">' + "\n" +
+            '        <h3 class="dialog-title">My Dialog</h3>' + "\n" +
+            '    </div>' + "\n" +
+            '    <div class="dialog-body">' + "\n" +
+            '        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam aspernatur, delectus deserunt earum error fugit iusto qui sit ut veniam. Alias at cupiditate enim est et iste necessitatibus neque ut.' + "\n" +
+            '    </div>' + "\n" +
+            '    <div class="dialog-footer">' + "\n" +
+            '        <button class="btn btn-flat text-blue-6" type="button" data-toggle="dialog">' + "\n" +
+            '            Cancel' + "\n" +
+            '        </button>' + "\n" +
+            '        <button class="btn btn-flat text-blue-6" type="button">' + "\n" +
+            '            Confirm' + "\n" +
+            '        </button>' + "\n" +
             '    </div>' + "\n" +
             '</div>'
         ),
@@ -52,26 +48,22 @@ angular.module('docs').controller('components.dialogsCtrl', function ($scope) {
             '<button class="btn bg-blue-6" type="button" data-toggle="dialog" data-target="#dialog3">' + "\n" +
             '    Open Dialog' + "\n" +
             '</button>' + "\n" +
-            '<div id="dialog3" class="dialog">' + "\n" +
-            '    <div class="dialog-content xs-col-10 sm-col-8 md-col-6">' + "\n" +
-            '        <div class="dialog-header">' + "\n" +
-            '            <h3 class="dialog-title">My Dialog</h3>' + "\n" +
-            '        </div>' + "\n" +
-            '        <div class="dialog-body">' + "\n" +
-            '            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam aspernatur, delectus deserunt earum error fugit iusto qui sit ut veniam. Alias at cupiditate enim est et iste necessitatibus neque ut.' + "\n" +
-            '        </div>' + "\n" +
-            '        <div class="dialog-footer">' + "\n" +
-            '            <button class="btn btn-flat text-blue-6" type="button" data-toggle="dialog" data-target="#dialog4">' + "\n" +
-            '                Open Dialog Over' + "\n" +
-            '            </button>' + "\n" +
-            '        </div>' + "\n" +
+            '<div id="dialog3" class="dialog xs-col-10 sm-col-8 md-col-6">' + "\n" +
+            '    <div class="dialog-header">' + "\n" +
+            '        <h3 class="dialog-title">My Dialog</h3>' + "\n" +
+            '    </div>' + "\n" +
+            '    <div class="dialog-body">' + "\n" +
+            '        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam aspernatur, delectus deserunt earum error fugit iusto qui sit ut veniam. Alias at cupiditate enim est et iste necessitatibus neque ut.' + "\n" +
+            '    </div>' + "\n" +
+            '    <div class="dialog-footer">' + "\n" +
+            '        <button class="btn btn-flat text-blue-6" type="button" data-toggle="dialog" data-target="#dialog4">' + "\n" +
+            '            Open Dialog Over' + "\n" +
+            '        </button>' + "\n" +
             '    </div>' + "\n" +
             '</div>' + "\n" +
             '<div id="dialog4" class="dialog">' + "\n" +
-            '    <div class="dialog-content">' + "\n" +
-            '        <div class="dialog-body">' + "\n" +
-            '            Hello World!' + "\n" +
-            '        </div>' + "\n" +
+            '    <div class="dialog-body">' + "\n" +
+            '        Hello World!' + "\n" +
             '    </div>' + "\n" +
             '</div>'
         )

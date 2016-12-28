6c00479ec019770094daf8efeed8a2446b139efa
Caio Eduardo <caioedut@gmail.com>
2016-12-27 23:36:00 -0200

    Fix

--- js/plugins/cem.toast.js
+++ js/plugins/cem.toast.js
@@ -13,7 +13,7 @@
         this.options = extend({}, Toast.DEFAULTS, options || {});
         this.options.message = message;
 
-        this.parent = this.options.parent == 'string' ? document.querySelector(this.options.parent) : this.options.parent;
+        this.parent = typeof this.options.parent == 'string' ? document.querySelector(this.options.parent) : this.options.parent;
         this.body = this.parent.querySelector(':scope > .layout-toast');
 
         if (!this.body) {

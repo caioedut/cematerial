cd5cf9884ad3c14e5ff3f0dad395c374eb08144b
Caio Eduardo <caioedut@gmail.com>
2017-04-19 18:22:12 -0300

    Drop tabs swipe support

--- js/plugins/cem.tabs.js
+++ js/plugins/cem.tabs.js
@@ -10,20 +10,28 @@
     // CLASS
 
     var Tabs = function (el, options) {
-        this.el = el.closest('.tabs');
+        this.el = el;
         this.options = extend({}, Tabs.DEFAULTS, el.dataset, options || {});
 
-        this.list = this.el.querySelector('.tabs-nav');
-        this.content = this.el.querySelectorAll('.tabs-list > .tab-content');
+        this.nav = this.el.closest('.tabs-nav');
+        this.content = document.querySelector(this.options.target);
 
-        if (this.el['cem.tabs']) {
-            this.bar = this.el['cem.tabs'].bar;
-            this.updateBar();
-        } else {
-            this.bar = document.createElement('div');
-            this.bar.classList.add('tabs-bar');
-            this.updateBar();
-            this.list.insertBefore(this.bar, this.list.firstChild);
+        if (this.content) {
+            this.list = this.content.closest('.tabs-list');
+        }
+
+        // BAR
+        if (this.nav) {
+            this.bar = this.nav.querySelector('.tabs-bar');
+
+            if (this.bar) {
+                this.updateBar();
+            } else {
+                this.bar = document.createElement('div');
+                this.bar.classList.add('tabs-bar');
+                this.updateBar();
+                this.nav.insertBefore(this.bar, this.nav.firstChild);
+            }
         }
 
         this.el['cem.tabs'] = this;
@@ -40,47 +48,8 @@
     };
 
     Tabs.prototype.show = function (_relatedTarget) {
+        var that = this;
         var e; // Event handler
-
-        if (!_relatedTarget) {
-            return;
-        }
-
-        var handler = _relatedTarget;
-
-        var target,
-            nav;
-
-        if (handler.matches('.tab-content')) {
-            target = handler;
-
-            var target_index = 0;
-            this.el.querySelectorAll('.tab-content').forEach(function (node, i) {
-                target_index = node === target ? i : target_index;
-            });
-
-            this.list.querySelectorAll('[data-toggle="tab"]').forEach(function (node, i) {
-                if (document.querySelector(node.dataset.target) === target || i == target_index) {
-                    nav = node;
-                }
-            });
-        } else {
-            target = document.querySelector(handler.dataset.target);
-            nav = handler;
-
-            if (!target) {
-                // Get tab content (target panel) by index
-                var nav_index = 0;
-                this.list.querySelectorAll('[data-toggle="tab"]').forEach(function (node, i) {
-                    nav_index = node === nav ? i + 1 : nav_index;
-                });
-
-                target = this.el.querySelector('.tab-content:nth-child(' + nav_index + ')');
-            }
-        }
-
-        // Close others openned
-        this.hide();
 
         // Event Before Show
         e = new Event('cem.tabs.beforeShow', {bubbles: true, cancelable: true, composed: true});
@@ -88,9 +57,33 @@
         this.el.dispatchEvent(e);
 
         // Show
-        nav.classList.add('tab-active');
-        target.classList.add('tab-visible');
-        this.updateBar();
+        if (this.content) {
+            // Hide others navs
+            if (this.nav) {
+                this.el.siblings().forEach(function (node) {
+                    if (node.classList.contains('tab-active')) {
+                        var init = node['cem.tabs'] || new Tabs(node);
+                        init.hide(that);
+                    }
+                });
+            }
+
+            // Hide Others Contents
+            if (this.list) {
+                this.content.siblings().forEach(function (node) {
+                    if (node.classList.contains('tab-visible')) {
+                        node.classList.remove('tab-visible');
+                    }
+                });
+            }
+
+            // Tab Nav
+            this.el.classList.add('tab-active');
+            this.updateBar();
+
+            // Tab Content
+            this.content.classList.add('tab-visible');
+        }
 
         // Event Show
         e = new Event('cem.tabs.show', {bubbles: true, cancelable: true, composed: true});
@@ -107,8 +100,7 @@
         this.el.dispatchEvent(e);
 
         // Hide
-        this.list.querySelector('.tab-active').classList.remove('tab-active');
-        this.el.querySelector('.tab-content.tab-visible').classList.remove('tab-visible');
+        this.el.classList.remove('tab-active');
 
         // Event Hide
         e = new Event('cem.tabs.hide', {bubbles: true, cancelable: true, composed: true});
@@ -117,23 +109,17 @@
     };
 
     Tabs.prototype.updateBar = function () {
-        var active = this.list.querySelector('.tab-active');
+        var active = this.nav.querySelector('.tab-active');
 
-        var pos = {left: active.offsetLeft - this.list.scrollLeft};
-        var scroll = this.list.scrollLeft;
+        var pos = {left: active.offsetLeft - this.nav.scrollLeft};
+        var scroll = this.nav.scrollLeft;
 
         var left = scroll + pos.left;
 
-        if (pos.left + active.offsetWidth > this.list.offsetWidth) {
-            this.list.scrollLeft = left - this.list.offsetWidth + active.offsetWidth;
-            // this.$list.animate({
-            //     scrollLeft: left - this.$list.offsetWidth + $active.offsetWidth
-            // }, 200);
+        if (pos.left + active.offsetWidth > this.nav.offsetWidth) {
+            this.nav.scrollLeft = left - this.nav.offsetWidth + active.offsetWidth;
         } else if (pos.left < 0) {
-            this.list.scrollLeft = left;
-            // this.$list.animate({
-            //     scrollLeft: left
-            // }, 200);
+            this.nav.scrollLeft = left;
         }
 
         // Update bar css
@@ -147,83 +133,83 @@
     // Events
     document
         .on('click', '[data-toggle="tab"]', function () {
-            var target = this.closest('.tabs');
-            var init = new Tabs(target, this.dataset);
+            // var target = this.closest('.tabs');
+            var init = new Tabs(this, this.dataset);
             init.show(this);
         })
-        .on('swipestart', '.tabs:not(.tabs-noswipe) .tabs-list', function () {
-            var tabs = this.closest('.tabs');
-            var init = tabs['cem.dropdown'] || new Tabs(tabs);
-
-            if (init.options.swipe && init.options.swipe != '0') {
-                init.bar.classList.add('no-transition');
-                this.querySelector('.tab-visible').classList.add('no-transition');
-
-                // GET TRANSLATE X VALUE
-                init.bar.dataset.translateX = init.bar.style.transform.replace(/\D/g, '');
-            }
-        })
-        .on('swipemove', '.tabs:not(.tabs-noswipe) .tabs-list', function (e) {
-            var is_horizontal = Math.abs(e.swipeOffsetX) > Math.abs(e.swipeOffsetY);
-            var is_parent_scrollable = false;
-
-            // Check parent scrollable
-            e.target.parentsUntil(this).forEach(function (node) {
-                if (node.scrollWidth > node.offsetWidth) {
-                    is_parent_scrollable = true;
-                }
-            });
-
-            if (is_horizontal && !is_parent_scrollable) {
-                var active = this.querySelector('.tab-visible');
-                var bar = this.closest('.tabs').querySelector('.tabs-bar');
-
-                e.preventDefault();
-
-                // Move tab content
-                active.style.marginLeft = e.swipeOffsetX;
-
-                // Move tab bar
-                var translateX = bar.dataset.translateX - (e.swipeOffsetX / this.offsetWidth * 100);
-                bar.style.transform = 'translateX(' + translateX + 'px)';
-            }
-        })
-        .on('swipeend', '.tabs:not(.tabs-noswipe) .tabs-list', function (e) {
-            var is_horizontal = Math.abs(e.swipeOffsetX) > Math.abs(e.swipeOffsetY);
-            var is_parent_scrollable = false;
-
-            // Check parent scrollable
-            e.target.parentsUntil(this).forEach(function (node) {
-                if (node.scrollWidth > node.offsetWidth) {
-                    is_parent_scrollable = true;
-                }
-            });
-
-            var active = this.querySelector('.tab-visible');
-            var bar = this.closest('.tabs').querySelector('.tabs-bar');
-
-            var offset_start = active.offsetWidth * 0.3;
-            var new_active;
-
-            if (is_horizontal && !is_parent_scrollable) {
-                if (Math.abs(e.swipeOffsetX) > offset_start) {
-                    if (e.swipeOffsetX > 0) {
-                        new_active = active.previousElementSibling;
-                    } else {
-                        new_active = active.nextElementSibling;
-                    }
-                }
-            }
-
-            bar.classList.remove('no-transition');
-            active.classList.remove('no-transition');
-
-            new_active = new_active || active;
-            this.closest('.tabs')['cem.tabs'].show(new_active);
-
-            // Reset tab content
-            active.style.marginLeft = '';
-        })
+    // .on('swipestart', '.tabs:not(.tabs-noswipe) .tabs-list', function () {
+    //     var tabs = this.closest('.tabs');
+    //     var init = tabs['cem.dropdown'] || new Tabs(tabs);
+    //
+    //     if (init.options.swipe && init.options.swipe != '0') {
+    //         init.bar.classList.add('no-transition');
+    //         this.querySelector('.tab-visible').classList.add('no-transition');
+    //
+    //         // GET TRANSLATE X VALUE
+    //         init.bar.dataset.translateX = init.bar.style.transform.replace(/\D/g, '');
+    //     }
+    // })
+    // .on('swipemove', '.tabs:not(.tabs-noswipe) .tabs-list', function (e) {
+    //     var is_horizontal = Math.abs(e.swipeOffsetX) > Math.abs(e.swipeOffsetY);
+    //     var is_parent_scrollable = false;
+    //
+    //     // Check parent scrollable
+    //     e.target.parentsUntil(this).forEach(function (node) {
+    //         if (node.scrollWidth > node.offsetWidth) {
+    //             is_parent_scrollable = true;
+    //         }
+    //     });
+    //
+    //     if (is_horizontal && !is_parent_scrollable) {
+    //         var active = this.querySelector('.tab-visible');
+    //         var bar = this.closest('.tabs').querySelector('.tabs-bar');
+    //
+    //         e.preventDefault();
+    //
+    //         // Move tab content
+    //         active.style.marginLeft = e.swipeOffsetX;
+    //
+    //         // Move tab bar
+    //         var translateX = bar.dataset.translateX - (e.swipeOffsetX / this.offsetWidth * 100);
+    //         bar.style.transform = 'translateX(' + translateX + 'px)';
+    //     }
+    // })
+    // .on('swipeend', '.tabs:not(.tabs-noswipe) .tabs-list', function (e) {
+    //     var is_horizontal = Math.abs(e.swipeOffsetX) > Math.abs(e.swipeOffsetY);
+    //     var is_parent_scrollable = false;
+    //
+    //     // Check parent scrollable
+    //     e.target.parentsUntil(this).forEach(function (node) {
+    //         if (node.scrollWidth > node.offsetWidth) {
+    //             is_parent_scrollable = true;
+    //         }
+    //     });
+    //
+    //     var active = this.querySelector('.tab-visible');
+    //     var bar = this.closest('.tabs').querySelector('.tabs-bar');
+    //
+    //     var offset_start = active.offsetWidth * 0.3;
+    //     var new_active;
+    //
+    //     if (is_horizontal && !is_parent_scrollable) {
+    //         if (Math.abs(e.swipeOffsetX) > offset_start) {
+    //             if (e.swipeOffsetX > 0) {
+    //                 new_active = active.previousElementSibling;
+    //             } else {
+    //                 new_active = active.nextElementSibling;
+    //             }
+    //         }
+    //     }
+    //
+    //     bar.classList.remove('no-transition');
+    //     active.classList.remove('no-transition');
+    //
+    //     new_active = new_active || active;
+    //     this.closest('.tabs')['cem.tabs'].show(new_active);
+    //
+    //     // Reset tab content
+    //     active.style.marginLeft = '';
+    // })
     ;
 
 }();

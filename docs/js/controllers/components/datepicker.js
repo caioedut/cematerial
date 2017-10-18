a5b8b68ab6dfb825ffdf814766a7bb5a48d5e2f5
Caio Eduardo <caioedut@gmail.com>
2017-10-18 17:10:27 -0200

    Datepicker: min date and max date

--- docs/js/controllers/components/datepicker.js
+++ docs/js/controllers/components/datepicker.js
@@ -7,6 +7,9 @@ angular.module('docs').controller('components.datepickerCtrl', function ($scope)
         ),
         colors: (
             '<input class="input" value="2017-01-01" placeholder="Focus me" readonly data-toggle="datepicker" data-color="red-6"/>'
-        )
+        ),
+        min_max: (
+            '<input class="input" value="2017-01-15" readonly data-toggle="datepicker" data-min="-7" data-max="10"/>'
+        ),
     }
 });

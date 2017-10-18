2ebf28177b24b0fa9775d9dc37a62b90c995c81a
Caio Eduardo <caioedut@gmail.com>
2017-10-18 17:23:05 -0200

    Fix

--- docs/js/controllers/components/datepicker.js
+++ docs/js/controllers/components/datepicker.js
@@ -3,13 +3,13 @@ angular.module('docs').controller('components.datepickerCtrl', function ($scope)
 
     $scope.codes = {
         basic: (
-            '<input class="input" value="2017-01-01" readonly data-toggle="datepicker"/>'
+            '<input class="input" value="2017-01-01" size="10" readonly data-toggle="datepicker"/>'
         ),
         colors: (
-            '<input class="input" value="2017-01-01" placeholder="Focus me" readonly data-toggle="datepicker" data-color="red-6"/>'
+            '<input class="input" value="2017-01-01" size="10" placeholder="Focus me" readonly data-toggle="datepicker" data-color="red-6"/>'
         ),
         min_max: (
-            '<input class="input" placeholder="Focus me" readonly data-toggle="datepicker" data-min="-7" data-max="10"/>'
+            '<input class="input" size="10" placeholder="Focus me" readonly data-toggle="datepicker" data-min="-7" data-max="10"/>'
         )
     }
 });
